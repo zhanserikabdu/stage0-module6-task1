9915987366e986936132687f3359d088518c83b9
NANO <File changed>
2022-08-28 00:31:16 +0600

    1 submission

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,19 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+private String color;
+private int numberOfPaws;
+private boolean hasFur;
+
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+       this.numberOfPaws = numberOfPaws;
+      this.hasFur = hasFur;
+   }
+  public String getDescription() {
+       String result="This animal is mostly " + color + ". It has " +numberOfPaws+
+               (numberOfPaws == 1 ? " paw" : " paws") + " and " + (hasFur ? "a" : "no") + " fur.";
+       return result;
+    }
 
 }
--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,16 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal {
 
+    public Bird() {
+        super("blue", 2, false);
+    }
+
+    @Override
+    public String getDescription() {
+        String description = super.getDescription();
+        return description + " Moreover, it has 2 wings and can fly.";
+    }
 }
+
+
